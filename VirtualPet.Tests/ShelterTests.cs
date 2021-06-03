5bea88b0ad8deba587d8b29f60f6047d92882b97
DaynaFairchild <84334081+DaynaFairchild@users.noreply.github.com>
2021-06-03 13:03:32 -0400

    beginning of shelter

---
+++ VirtualPet.Tests/ShelterTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VirtualPet.Tests
+{
+    public class ShelterTests
+    {
+        Shelter testShelter;
+        public ShelterTests()
+        {
+            testShelter = new Shelter();
+        }
+
+        [Fact]
+        public void GetPetCount_Should_Return_Pet_Count()
+        {
+            Assert.Equal(4, petList.Count);
+        }
+
+        [Fact]
+        public void AddPet_Should_Increase_Pet_Count_By_One()
+        {
+            Pet myPet = new Pet();
+            testShelter.AddPet(myPet);
+            int petCount = testShelter.getPetCount();
+            
+            Assert.Equal(1 , petCount);
+        }
+
+
+    }
+}
---
+++ VirtualPet/Shelter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    public class Shelter
+    {
+
+        public List<Pet> petList { get; set; }
+
+        public Shelter()
+        {
+            petList = new List<Pet>();
+
+        }
+
+        public int GetPetCount(petList)
+        {
+            return petList.Count;
+        }
+    
+
+
+    }
+}
