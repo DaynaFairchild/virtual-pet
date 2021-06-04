73072dc804ccea4cd5eb27996fff1ed7f49a1fa0
joshuabharper7 <84295436+joshuabharper7@users.noreply.github.com>
2021-06-03 22:49:31 -0400

    Added individualInteractions and updated some code

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 
 namespace VirtualPet
-{
+{   //Create mathod for showing current pets
+    //fix bug for after new pet is added for referencing list
     class Program
     {
         public static Shelter petzForDayz = new Shelter();
@@ -22,81 +23,14 @@ namespace VirtualPet
 
             StartingIntro();
 
-            mainMenu();
+            bool continueGame = true;
+            while (continueGame)
+            {
+                mainMenu();
+            }
+            
 
-            //bool keepPlaying = true;
-            //while (keepPlaying)
-            //{
-            //    if (userPet.Health == 0)
-            //    {
-            //        Console.Clear();
-            //        Console.WriteLine($"WOW, {userPet.Name}'s Health Reached 0, your {userPet.Species} has been seized by Animal Protective League!");
-            //        Console.WriteLine("Would you like to play again? y/n");
-            //        string playAgain = Console.ReadLine().ToLower();
-            //        if (playAgain == "y")
-            //        {
-            //            Console.Clear();
-            //            userPet = new Pet();
 
-            //            AddNewPetIntro(userPet);
-            //        }
-            //        else
-            //        {
-            //            Console.Clear();
-            //            Console.WriteLine("Thanks for playing.");
-            //            break;
-            //        }
-            //    }
-            //    Console.Clear();
-            //    Console.WriteLine($"Hunger: {userPet.Hunger} | Boredom: {userPet.Boredom} | Health: {userPet.Health}\n");
-            //    Console.WriteLine($"What would you like to do with {userPet.Name}?");
-            //    Console.WriteLine($"1. Feed {userPet.Name}");
-            //    Console.WriteLine($"2. Play with {userPet.Name}");
-            //    Console.WriteLine($"3. Take {userPet.Name} to the vet :(");
-            //    Console.WriteLine("4. Exit");
-
-            //    string userChoice = Console.ReadLine();
-            //    switch (userChoice)
-            //    {
-            //        case "1":
-            //            Console.Clear();
-            //            userPet.Feed();
-            //            continueKey();
-            //            Console.Clear();
-            //            Console.WriteLine($"Hunger: {userPet.Hunger} | Boredom: {userPet.Boredom} | Health: {userPet.Health}\n");
-            //            Console.WriteLine($"{userPet.Name}'s Hunger decreased to {userPet.Hunger}.");
-            //            continueKey();
-            //            break;
-            //        case "2":
-            //            Console.Clear();
-            //            userPet.Play();
-            //            continueKey();
-            //            Console.Clear();
-            //            Console.WriteLine($"Hunger: {userPet.Hunger} | Boredom: {userPet.Boredom} | Health: {userPet.Health}\n");
-            //            Console.WriteLine($"You played a game with {userPet.Name}. Hooray!");
-            //            Console.WriteLine($"{userPet.Name}'s Boredom decresed to {userPet.Boredom}, Hunger increased to {userPet.Hunger}, and Health increased to {userPet.Health}.");
-            //            continueKey();
-            //            break;
-            //        case "3":
-            //            userPet.SeeDoctor();
-            //            Console.Clear();
-            //            Console.WriteLine($"Hunger: {userPet.Hunger} | Boredom: {userPet.Boredom} | Health: {userPet.Health}\n");
-            //            Console.WriteLine($"You took {userPet.Name} for a checkup at the vet.");
-            //            Console.WriteLine($"{userPet.Name}'s Health increased to {userPet.Health}.");
-            //            continueKey();
-            //            break;
-            //        case "4":
-            //            Console.WriteLine($"Thanks for playing! {userPet.Name} has enjoyed your company!");
-            //            keepPlaying = false;
-            //            break;
-            //        default:
-            //            Console.WriteLine("Please choose from options 1-4. Press any key to be able to choose again.");
-            //            Console.ReadKey();
-            //            break;
-
-            //    }
-            //    userPet.Tick();
-            //}
 
         }
 
@@ -120,7 +54,7 @@ namespace VirtualPet
         }
         public static  void AddNewPet()
         {
-            //Console.WriteLine("Hello! Welcome to Virtual Pets\n");
+            Console.Clear();
             Console.WriteLine("What species is this pet?"); 
             string species = Console.ReadLine();
             
@@ -134,21 +68,22 @@ namespace VirtualPet
             Console.WriteLine($"Like any pet, {name} needs your love and attention.");
             Console.WriteLine($"The passage of time (each time you return to the menu) will affect {name}'s stats.");
             Console.WriteLine($"You will need to interact with and care for {name} to keep them happy and healthy.\n");
-            Console.WriteLine($"Press any key to begin your new friendship with your adopted {species}.");
-            Console.ReadKey();
+            continueKey();
         }
-        public static void RemovePet(int chosenPet)
+        public static void RemovePet(Pet chosenPet)
         {
-            petzForDayz.petList.RemoveAt(chosenPet-1);
+            petzForDayz.petList.Remove(chosenPet);
         }
-        public static int ChoosePet()
+        public static Pet ChoosePet()
         {
+            Console.Clear();
             Console.WriteLine("Please choose a pet (type the number):\n\n");
             foreach (Pet pet in petzForDayz.petList)
             {
                 Console.WriteLine($"{pet.Id}. {pet.Name} ({pet.Species})\n");
             }
-            int chosenPet = Convert.ToInt32(Console.ReadLine());
+            int petNumber = Convert.ToInt32(Console.ReadLine());
+            Pet chosenPet = petzForDayz.petList[petNumber - 1];
             return chosenPet;
         }
 
@@ -156,6 +91,7 @@ namespace VirtualPet
         {
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
+            Console.Clear();
         }
 
         public static void mainMenu()
@@ -176,6 +112,8 @@ namespace VirtualPet
                     RemovePet(ChoosePet());
                         break;
                 case "3":
+                    IndividualInteraction(ChoosePet());
+                    break;
                 case "4":
                 case "5":
                 default:
@@ -185,6 +123,71 @@ namespace VirtualPet
 
             }
            
+        }
+        public static void IndividualInteraction(Pet userPet)
+        {
+            bool keepPlaying = true; 
+            while (keepPlaying)
+            {
+                if (userPet.Health == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"WOW, {userPet.Name}'s Health Reached 0, your {userPet.Species} has been seized by Animal Protective League!");
+                    Console.WriteLine("Please DO NOT mistreat the pets!");
+                    RemovePet(userPet);
+                    continueKey();
+                    break;
+                }
+                Console.Clear();
+                Console.WriteLine($"Hunger: {userPet.Hunger} | Boredom: {userPet.Boredom} | Health: {userPet.Health}\n");
+                Console.WriteLine($"What would you like to do with {userPet.Name}?");
+                Console.WriteLine($"1. Feed {userPet.Name}");
+                Console.WriteLine($"2. Play with {userPet.Name}");
+                Console.WriteLine($"3. Take {userPet.Name} to the vet :(");
+                Console.WriteLine("4. Exit");
+
+                string userChoice = Console.ReadLine();
+                switch (userChoice)
+                {
+                    case "1":
+                        Console.Clear();
+                        userPet.Feed();
+                        continueKey();
+                        Console.Clear();
+                        Console.WriteLine($"Hunger: {userPet.Hunger} | Boredom: {userPet.Boredom} | Health: {userPet.Health}\n");
+                        Console.WriteLine($"{userPet.Name}'s Hunger decreased to {userPet.Hunger}.");
+                        continueKey();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        userPet.Play();
+                        continueKey();
+                        Console.Clear();
+                        Console.WriteLine($"Hunger: {userPet.Hunger} | Boredom: {userPet.Boredom} | Health: {userPet.Health}\n");
+                        Console.WriteLine($"You played a game with {userPet.Name}. Hooray!");
+                        Console.WriteLine($"{userPet.Name}'s Boredom decresed to {userPet.Boredom}, Hunger increased to {userPet.Hunger}, and Health increased to {userPet.Health}.");
+                        continueKey();
+                        break;
+                    case "3":
+                        userPet.SeeDoctor();
+                        Console.Clear();
+                        Console.WriteLine($"Hunger: {userPet.Hunger} | Boredom: {userPet.Boredom} | Health: {userPet.Health}\n");
+                        Console.WriteLine($"You took {userPet.Name} for a checkup at the vet.");
+                        Console.WriteLine($"{userPet.Name}'s Health increased to {userPet.Health}.");
+                        continueKey();
+                        break;
+                    case "4":
+                        Console.WriteLine($"Thanks for playing! {userPet.Name} has enjoyed your company!");
+                        keepPlaying = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose from options 1-4. Press any key to be able to choose again.");
+                        Console.ReadKey();
+                        break;
+
+                }
+                userPet.Tick();
+            }
         }
        
     }
